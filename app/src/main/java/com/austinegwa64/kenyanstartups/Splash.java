67a18f4207a57c199be4627f16d859e56a1d15a2
Austine Gwa <austinegwa64@gmail.com>
2018-06-27 07:47:33 +0300

    Added a splash screen to the app

--- app/src/main/java/com/austinegwa64/kenyanstartups/Splash.java
+++ app/src/main/java/com/austinegwa64/kenyanstartups/Splash.java
@@ -2,13 +2,25 @@ package com.austinegwa64.kenyanstartups;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
         setContentView(R.layout.activity_splash);
 
         Thread loadHomeActivityThread = new Thread(){
@@ -19,6 +31,7 @@ public class Splash extends AppCompatActivity {
                     sleep(3000);
                     Intent loadHomeActivity = new Intent(getApplicationContext(), Home.class);
                     startActivity(loadHomeActivity);
+                    finish();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -26,5 +39,6 @@ public class Splash extends AppCompatActivity {
             }
         };
         loadHomeActivityThread.start();
+
     }
 }
